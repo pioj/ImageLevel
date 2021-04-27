10bcd1919e724213b27aed4e6147308c34a882c3
pioj <piojete@gmail.com>
2021-04-28 01:28:23 +0200

    Next Upgrade. 1st, ExtractTiles revamped!
    He re-hecho de cero la herramienta con los nuevos conocimientos que ya tengo sobre FilePanels y demás...
    
    La herramienta ahora permite elegir del Project, o bien cargar una Imagen externa y genera de ahí una textura en memoria.
    Como extra, ahora también se muestra una preview de la Textura elegida.
    
    TO-DO: hacer revamp de las demás herramientas, y añadir todo lo nuevo que estoy haciendo de soporte para mapas de TILED...

--- Assets/Editor/Editor_ExtractTiles_Tex2D.cs
+++ Assets/Editor/Editor_ExtractTiles_Tex2D.cs
@@ -1,44 +1,84 @@
-﻿using UnityEditor;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
 
 namespace pioj.ImageLevel
 {
-
-    public class Editor_ExtractTiles_Tex2D : ScriptableWizard
+    public class Editor_ExtractTiles_Tex2D : EditorWindow
     {
-
-        public Texture2D Image;
-
-
-        [MenuItem("Tools/ImageLevel/Step 1. Create TileSet/From Texture2D...")]
-        static void CreateWizard()
+        public Texture2D tex;
+        private static string path;
+        
+        // Add menu named "My Window" to the Window menu
+        [MenuItem("Tools/ImageLevel/Step 1. Create Palette from Texture2D...")]
+        static void Init()
         {
-            ScriptableWizard.DisplayWizard<Editor_ExtractTiles_Tex2D>("Create TileSet from Texture2D", "Create Tileset");
-            //If you don't want to use the secondary button simply leave it out:
-            //ScriptableWizard.DisplayWizard<WizardCreateLight>("Create Light", "Create");
-
+            // Get existing open window or if none, make a new one:
+            Editor_ExtractTiles_Tex2D window = (Editor_ExtractTiles_Tex2D)EditorWindow.GetWindow(typeof(Editor_ExtractTiles_Tex2D));
+            window.titleContent = new GUIContent("Extract Palette...");
+            window.maxSize = new Vector2(300, 300);
+            window.Show();
         }
 
-        void OnWizardCreate()
+
+
+        void OnGUI()
         {
-            if (Image != null) GeneratePalette();
+            if (!tex) EditorGUILayout.HelpBox("Please Select or Load a Texture file.",MessageType.Warning);
+            
+            EditorGUILayout.Space(10);
+            EditorGUILayout.BeginHorizontal(GUILayout.ExpandWidth(true));
+            EditorGUIUtility.labelWidth = 15;
+            if (GUILayout.Button("..."))
+            {
+                path = BuscaTexture();
+                if (File.Exists(path))
+                {
+                    var file = File.ReadAllBytes(path);
+                    tex = new Texture2D(2, 2);
+                    tex.LoadImage(file);
+                    tex.name = path;
+                    tex.filterMode = FilterMode.Point;
+                }
+            }
+            EditorGUILayout.LabelField("OR", GUILayout.MaxWidth(25));
+            //
+            var obj = EditorGUILayout.ObjectField("", tex, typeof(Texture2D), false, GUILayout.Height(17));
+            if (obj) tex = (Texture2D) obj;
+            //Image.filterMode = FilterMode.Point;
+            EditorGUILayout.EndHorizontal();
+
+            if (tex)
+            {
+                EditorGUILayout.Space(5);
+                if (GUILayout.Button("Generate Palette", GUILayout.ExpandWidth(true)))
+                {
+                    GeneratePalette();
+                }
+                EditorGUILayout.LabelField("Preview: " + tex.name);
+                EditorGUI.DrawPreviewTexture(new Rect(5,60,base.position.width-10, base.position.height-10) , tex, null, ScaleMode.ScaleToFit);
+            }
         }
 
-        void OnWizardUpdate()
+        private static string BuscaTexture()
         {
-            helpString = "Please select a Texture2D first!";
+            path = EditorUtility.OpenFilePanelWithFilters("Select Texture...", Application.dataPath,
+                new string[] {"png", "png"});
+            return path;
         }
-
-        // When the user presses the "Apply" button OnWizardOtherButton is called.
-        void OnWizardOtherButton() { }
-
-
+        
+        
         //el metodo
         void GeneratePalette()
         {
-            Color32[] AllPixels = Image.GetPixels32();
+            Color32[] AllPixels = tex.GetPixels32();
 
-            string pathname = AssetDatabase.GenerateUniqueAssetPath("Assets/Resources/" + Image.name.ToString() + "_Palette" + ".asset");
+            var pathname = EditorUtility.SaveFilePanelInProject("Save Palette Asset as...", "Palette_xxx", "asset", "asset");
+
+            //string pathname = AssetDatabase.GenerateUniqueAssetPath("Assets/Resources/" + tex.name.ToString() + "_Palette" + ".asset");
 
             SO_Color2Prefab tempAsset = CreateInstance<SO_Color2Prefab>();
             AssetDatabase.CreateAsset(tempAsset, pathname);
@@ -52,10 +92,7 @@ namespace pioj.ImageLevel
             AssetDatabase.SaveAssets();
             AssetDatabase.Refresh();
             EditorUtility.FocusProjectWindow();
-
         }
+        
     }
 }
-
-
-
